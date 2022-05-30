f4610bfee31baa229c72ce9b99511f2c7c35c086
Vladislav <atlantis437@yandex.ru>
2022-05-30 14:35:07 +0300

    feat form

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+
+
+
+function openPopup() {
+    
+    let container = document.querySelector('container');
+    let popup = container.querySelector('popup');
+    let formElement = popup.querySelector('popup__container');
+    let editElement = container.querySelector('profile__edit-button');
+    let closeElement = popup.querySelector('popup__close-button');
+
+    if (editElement = true)
+
+    
+    popup.classlist.add('popup_opened');
+    
+
+
+}
+
+       
+
+  
+
+editElement.addEventListener('click', openPopup);
+openPopup();
