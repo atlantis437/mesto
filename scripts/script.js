c959911bd87664fab868e089a4e6a3e6cf57532c
Vladislav <atlantis437@yandex.ru>
2022-05-30 23:42:34 +0300

    feat nested

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,36 @@
 
-
-
 const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closeButton = document.querySelector('.popup__close-button');
 
-editButton.addEventListener('click', function() {
+
+function openPopup () {
     popup.classList.add('popup_open');
+}
+
+editButton.addEventListener('click', function() {
+    openPopup ();
 })
 
-closeButton.addEventListener('click', function() {
+function closePopup () {
     popup.classList.remove('popup_open');
+}
+
+closeButton.addEventListener('click', function() {
+    closePopup ();
 })
+
+const saveButton = document.querySelector('.popup__save-button');
+const nameInput = document.querySelector('.popup__form_name');
+const jobInput = document.querySelector('.popup__form_job');
+const UserName = document.querySelector('.profile__title');
+const UserJob = document.querySelector('.profile__subtitle');
+
+function editForm () {
+    UserName.textContent = nameInput.value;
+    UserJob.textContent = jobInput.value;
+    closePopup ();
+}
+
+saveButton.addEventListener('click', editForm);
+
