95780bd510f9fe5a820ade76642441a2ea2dfba2
Vladislav <atlantis437@yandex.ru>
2022-06-16 02:52:40 +0300

    feat template

--- scripts/script.js
+++ scripts/script.js
@@ -53,29 +53,64 @@ function editForm () {
 
 saveButton.addEventListener('click', editForm);
 
+
+const elements = document.querySelector('.elements');
+const cardTemplate = document.querySelector('#card').content;
 const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ];
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach(function (renderCard) {
+const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+
+cardElement.querySelector('.element__photo').src = renderCard.link;
+cardElement.querySelector('.element__title').textContent = renderCard.name;
+
+elements.append(cardElement);
+});
+
+
+
+
+const saveimgButton = document.querySelector('.popup__saveimg-button');
+const ImgnameInput = document.querySelector('.popup__img_name');
+const ImgsrcInput = document.querySelector('.popup__img_src');
+const ImgName = document.querySelector('.element__title');
+const ImgSrc = document.querySelector('.element__photo');
+
+
+function createCard (evt) {
+  evt.preventDefault();  
+  initialCards.unshift(ImgSrc, ImgName);  
+  ImgName.textContent = ImgnameInput.value;
+  ImgSrc.src = ImgsrcInput.value;
+  closePopupImg ();
+  
+}
+
+saveimgButton.addEventListener('click', createCard);
+
+
